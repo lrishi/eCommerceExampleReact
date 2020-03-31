de4161c40a77e87f4045dc27b196c2f778a6de36
Lovel Rishi <lovelworks@gmail.com>
2020-03-31 14:52:46 +0530

    Code to capture user login from firebase authentication object and store user data to firestore if user doesn't exist

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,36 @@ const config = {
     appId: "1:760673475655:web:dd8df56e8eb04a7540a562"
 };
 
+
+export const createUserProfileDocument = async (userAuth, ...additionalData) => {
+
+    if (userAuth == null) {
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
