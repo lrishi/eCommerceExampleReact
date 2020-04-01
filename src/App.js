83bc24a8778bd55aefad3e343f0906d161765368
Lovel Rishi <lovelworks@gmail.com>
2020-04-01 07:18:35 +0530

    Added redux and implemented userReducer in header component

--- src/App.js
+++ src/App.js
@@ -6,33 +6,28 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
 import Authenticator from './components/authenticator/authenticator.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { connect as connectRedux } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          }, () => { console.log(this.state); });
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+          );
 
         })
       } else {
-        this.setState({ currentUser: null }, () => { console.log('user logout', this.state); });
+        setCurrentUser(null);
       }
     });
   }
@@ -44,7 +39,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact={true} path='/' component={HomePage} />
           <Route exact={true} path='/shop' component={ShopPage} />
@@ -55,4 +50,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connectRedux(null, mapDispatchToProps)(App);
