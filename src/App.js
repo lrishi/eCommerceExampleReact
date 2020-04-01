3a02f95dddc7cab31739e3d2978a7bf05ccef7a5
Lovel Rishi <lovelworks@gmail.com>
2020-04-01 13:12:27 +0530

    Redirect to homepage on sign in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/header/header.component';
 import Authenticator from './components/authenticator/authenticator.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -43,15 +43,19 @@ class App extends React.Component {
         <Switch>
           <Route exact={true} path='/' component={HomePage} />
           <Route exact={true} path='/shop' component={ShopPage} />
-          <Route exact={true} path='/signin' component={Authenticator} />
+          <Route exact={true} path='/signin' render={() => this.props.currentUser ? (<Redirect to="/" />) : (<Authenticator />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connectRedux(null, mapDispatchToProps)(App);
+export default connectRedux(mapStateToProps, mapDispatchToProps)(App);
